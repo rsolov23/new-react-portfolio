73c08b0f77e730992cdd115438536b9972a74819
Rachel Solov <rsolov23@yahoo.com>
2021-12-21 17:47:47 -0600

    worked on resume component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,12 +3,26 @@ import { Document, Page } from "react-pdf";
 // import Particle from "./Particle";
 import { ResumeStyle } from "../../styles/ResumeStyles";
 import { PaperStyle } from "../../styles/ResumeStyles";
+import { AlignLeftStyle } from "../../styles/ResumeStyles";
+import { ButtonStyle } from "../../styles/ResumeStyles";
+
 export default function Resume() {
   return (
     <ResumeStyle>
-  
+      <ButtonStyle>
+        <a
+          align="center"
+          id="download"
+          href={require(`./test.pdf`).default}
+          download="test.pdf"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="name noselect">Download</button>
+        </a>
+      </ButtonStyle>
       <PaperStyle>
-        <h1>RACHEL SOlOV</h1>
+        <h1>RACHEL SOLOV</h1>
         <p>
           Cedar Park, TX 78613 | Phone: 830.385.6947 | Email:
           rachelswebdev@gmail.com
@@ -22,112 +36,145 @@ export default function Resume() {
         <h4>Summary</h4>
         <p>
           Dependable and dynamic Full Stack Web Developer with over ten years of
-          leadership experience. In the University of Texas Coding Bootcamp I
-          worked with a team to develop a MERN Stack application to help connect
-          people experiencing homelessness with community resources. Proficient
-          in React, CSS, HTML, Javascript, Express, MongoDb and MySQL. Seeking a
-          position to utilize my deep-rooted passion for learning as a motivated
-          self-starter who is detail oriented, excels in critical thinking and
-          has a knack for problem solving.
+          leadership experience. In the University of Texas
         </p>
+        <p>
+          {" "}
+          Coding Bootcamp I worked with a team to develop a MERN Stack
+          application to help connect people experiencing
+        </p>
+        <p>
+          {" "}
+          homelessness with community resources. Proficient in React, CSS, HTML,
+          Javascript, Express, MongoDb and MySQL.
+        </p>
+        <p>
+          {" "}
+          Seeking a position to utilize my deep-rooted passion for learning as a
+          motivated self-starter who is detail oriented, excels in
+        </p>
+        <p> critical thinking and has a knack for problem solving.</p>
         <h4>
           <strong>Technical Skills</strong>
         </h4>
-        <p>
-          <strong>Languages:</strong> HTML, CSS, JavaScript
-        </p>
-        <p>
-          <strong>Frameworks:</strong> React JS, Express JS, Node JS
-        </p>
-        <p>
-          <strong>Libraries:</strong> Bootstrap, Chakra UI, Bulma, jQuery
-        </p>
+        <AlignLeftStyle>
+          <p>
+            <strong>Languages:</strong> HTML, CSS, JavaScript
+          </p>
+          <p>
+            <strong>Frameworks:</strong> React JS, Express JS, Node JS
+          </p>
+          <p>
+            <strong>Libraries:</strong> Bootstrap, Chakra UI, Bulma, jQuery
+          </p>
+        </AlignLeftStyle>
         <h4>
           <strong>Projects</strong>
         </h4>
-        <p>
-          <strong>Helping Hands |Link: </strong>{" "}
-          https://helping-hands-deux.herokuapp.com/
-        </p>
-        <p>
-          <strong>Github:</strong> https://github.com/rsolov23/helping-hands{" "}
-        </p>
-        <ul>
-          <li>
-            {" "}
-            ◆ Helping Hands is an app to serve the less fortunate through grass
-            roots efforts of engaged community members
-          </li>
-          <li> ◆ Project Manager and led Front End Development</li>
-          <li> ◆ React, GraphQL, Express, MongoDB, Mongoose, Chakra UI</li>
-        </ul>
-        <p>
-          <strong>Jot It | Link: </strong> https://jotitgood.herokuapp.com/
-        </p>
-        <p>
-          <strong>Github:</strong>https://github.com/rsolov23/Jot-It
-        </p>
-        <ul>
-          <li>
-            ◆ Jot It is a note taking app with the option to edit, save and
-            delete any notes
-          </li>
-          <li>◆ Led Back End Development</li>
-          <li>
-            ◆ JavaScript, Handlebars, Bootstrap, Sequelize, Express, MySql2
-          </li>
-        </ul>
-        <p>
-          <strong>Gaminator | Link: </strong>{" "}
-          https://rsolov23.github.io/Gaminator/
-        </p>
-        <p>
-          <strong>Github:</strong>https://github.com/rsolov23/Gaminator
-        </p>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
-        {/* <a
-          align="center"
-          id="download"
-          href={require(`./test.pdf`).default}
-          download="test.pdf"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button className="name noselect">Download</button>
-        </a> */}
+        <AlignLeftStyle>
+          <p>
+            <strong>Helping Hands |Link: </strong>{" "}
+            https://helping-hands-deux.herokuapp.com/
+          </p>
+          <p>
+            <strong>Github:</strong> https://github.com/rsolov23/helping-hands{" "}
+          </p>
+          <ul>
+            <li>
+              {" "}
+              ◆ Helping Hands is an app to serve the less fortunate through
+              grass roots efforts of engaged community members
+            </li>
+            <li> ◆ Project Manager and led Front End Development</li>
+            <li> ◆ React, GraphQL, Express, MongoDB, Mongoose, Chakra UI</li>
+          </ul>
+          <p>
+            <strong>Jot It | Link: </strong> https://jotitgood.herokuapp.com/
+          </p>
+          <p>
+            <strong>Github:</strong>https://github.com/rsolov23/Jot-It
+          </p>
+          <ul>
+            <li>
+              ◆ Jot It is a note taking app with the option to edit, save and
+              delete any notes
+            </li>
+            <li>◆ Led Back End Development</li>
+            <li>
+              ◆ JavaScript, Handlebars, Bootstrap, Sequelize, Express, MySql2
+            </li>
+          </ul>
+          <p>
+            <strong>Gaminator | Link: </strong>{" "}
+            https://rsolov23.github.io/Gaminator/
+          </p>
+          <p>
+            <strong>Github:</strong>https://github.com/rsolov23/Gaminator
+          </p>
+          <ul>
+            <li>
+              ◆ Gaminator is a tool that will help those who have little to no
+              experience playing video games to find the
+            </li>
+            <li>right type of game for them to enjoy</li>
+            <li>◆ Project Manager and led Front End Development</li>
+            <li>◆ HTML, CSS, JavaScript, REST API</li>
+          </ul>
+        </AlignLeftStyle>
+        <h4>
+          <strong>Experience</strong>
+        </h4>
+        <AlignLeftStyle>
+          <p>
+            <strong>R&R Property Services: Co-Owner</strong> Cedar Park, TX May
+            2018-Present
+          </p>
+          <p>
+            Managed a team that provided property services ranging from vacation
+            rental management to cleaning{" "}
+          </p>
+          <p>services in Austin and surrounding areas.</p>
+          <ul>
+            <li>◆ Led hiring process and training of subcontractors</li>
+            <li>
+              ◆ Managed and monitored development of schedules for all employees
+            </li>
+            <li>
+              ◆ Maintained a 100% customer satisfaction rating by providing
+              quality customer service in a timely manner,
+            </li>
+            <li>ensuring client retention and company standards</li>
+          </ul>
+          <p>
+            <strong>
+              Trilogy Education(Full Stack Boot Camp): University of Texas-
+              Teacher Assistant
+            </strong>
+            November 2021-Present
+          </p>
+          <ul>
+            <li>◆ Support Instructor and fellow TA’s during lectures</li>
+            <li>
+              ◆ Answer students questions during class, break-out sessions and
+              during office hours by articulating clear and{" "}
+            </li>
+            <li> thorough responses that support student’s needs</li>
+          </ul>
+        </AlignLeftStyle>
+        <h4>
+          <strong>Education</strong>
+        </h4>
+        <AlignLeftStyle>
+          <p>
+            <strong>Full Stack Coding Bootcamp Certificate:</strong>University
+            of Texas at Austin, August 2021
+          </p>
+          <p>
+            An intensive 24 week long boot camp where I acquired the knowledge
+            and skills to build dynamic end-to-end web
+          </p>
+          <p> applications.</p>
+        </AlignLeftStyle>
       </PaperStyle>
     </ResumeStyle>
   );
--- src/styles/ResumeStyles.js
+++ src/styles/ResumeStyles.js
@@ -9,16 +9,42 @@ export const PaperStyle = styled.div`
   background-color: rgb(250, 249, 246);
   margin: auto;
   width: 700px;
-  height: 1000px;
+  height: 900px;
   border: 1px solid rgb(113, 121, 126);
   border-radius: 2px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
   text-align: center;
+  line-height: 0.1;
   p,
   li {
     font-size: 12px;
+    margin-left: 15px;
+    /* line-height: 0.1; */
   }
   ul {
     list-style: none;
   }
+
+  h4 {
+    font-size: 14px;
+  }
+  h1 {
+    font-size: 24px;
+    padding: 10px 10px;
+  }
+  p {
+    margin: 15px;
+  }
+  li {
+    margin-bottom: 15px;
+  }
+`;
+
+export const AlignLeftStyle = styled.div`
+  text-align: left;
+  margin: 15px;
+`;
+export const ButtonStyle = styled.div`
+  display: flex;
+  justify-content: center;
 `;
